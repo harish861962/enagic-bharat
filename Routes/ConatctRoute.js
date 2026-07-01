d03d3c06392f6d82adb57c1bb304c5841f710f3e
harish861962 <harishsaini1266@gmail.com>
2026-07-01 11:25:15 +0530

    Update backend API code

--- Routes/ConatctRoute.js
+++ Routes/ConatctRoute.js
@@ -35,8 +35,7 @@ router.post("/contact", async (req, res) => {
     //     pass: process.env.EMAIL_PASS,
     //   },
     // });
-
-   const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
@@ -44,12 +43,9 @@ router.post("/contact", async (req, res) => {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
 });
-await transporter.verify();
 
+await transporter.verify();
 console.log("SMTP Connected");
 
     await transporter.sendMail({
